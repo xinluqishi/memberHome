327d739671ae2ca2cc3488c9a00db777600e6c92
shikeyue <xinluqishi@126.com>
2018-06-13 13:05:19 +0800

    initial commit

---
+++ src/bean/Person.kt
@@ -0,0 +1,10 @@
+package bean
+
+import com.sun.jmx.snmp.Timestamp
+import java.util.*
+
+/**
+ * Created by shikeyue on 2018/6/12.
+ */
+
+class Person(val personId: String, val name: String, val sex: String, val accessDate: Date, val personSocialId: String)
